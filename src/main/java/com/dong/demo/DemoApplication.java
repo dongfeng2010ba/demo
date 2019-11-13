086eaf905dc448fd924f93d0ff78b8403f122171
dongfeng2010 <dongfeng2010@foxmail.com>
2019-11-13 10:59:04 +0800

    develop最简单的demo

--- src/main/java/com/dong/demo/DemoApplication.java
+++ src/main/java/com/dong/demo/DemoApplication.java
@@ -2,14 +2,13 @@ package com.dong.demo;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication
-@EnableAsync
-@EnableTransactionManagement
-@MapperScan("com.dong.demo.mapper")
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class DemoApplication {
 
     public static void main(String[] args) {
